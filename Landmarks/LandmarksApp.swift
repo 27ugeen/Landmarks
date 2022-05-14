b0ef7a81493bb4827f639bfde2907e7b307866ce
27ugeen <27ugeen@gmail.com>
2022-05-14 11:56:00 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by GiN Eugene on 14/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
